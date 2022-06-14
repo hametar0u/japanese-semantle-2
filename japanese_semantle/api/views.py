81c97fb810fb0b3d105d832e2b891f63f02cbdff
hametar0u <57603310+hametar0u@users.noreply.github.com>
2022-06-14 08:21:47 -0700

    load word vectors into the database

--- japanese_semantle/api/views.py
+++ japanese_semantle/api/views.py
@@ -3,10 +3,13 @@ from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from .serializers import TodoSerializer
 from rest_framework.response import Response
-from .models import Word
+from .models import Word, VectorWord, DailyKey
+# from celery import Celery
+# from celery.schedules import crontab
 
 import random
 import datetime
+import numpy as np
 
 from .models import Todo
 # Create your views here.
@@ -15,6 +18,8 @@ from .models import Todo
 class TodoListView(generics.ListAPIView):
   model = Todo
   serializer_class = TodoSerializer
+
+#Todo: update the daily word
 
 @api_view(['GET'])
 def load_daily_top_1000(request, key):
@@ -53,7 +58,50 @@ def load_daily_top_1000(request, key):
 
   return Response({"response": data})
 
+def sigmoid(x):
+    if x == 1:
+        return 100
+    return 100 * np.tanh(x)
+
+def score(a, b):
+    x = a * b
+    x = sum(x) / (np.linalg.norm(a) * np.linalg.norm(b))
+    x = round(x, 6)
+    return sigmoid(x)
+
 @api_view(['POST'])
 def evaluate_word(request, word):
+  daily_key = DailyKey.objects.all().last().key
+  daily_word = Word.objects.get(pk=daily_key)
+  top1000 = daily_word.top1000_set.all()
+  
+  is_top1000 = False
 
-  return Response(word)
+  for i, topword in enumerate(top1000):
+    if word == topword.top1000word:
+      obj = {
+        "word": word,
+        "score": topword.score,
+        "rank": 1000 - i
+      }
+      return Response(obj)
+
+  #calculate score if not top 1000
+  daily_word_vector = VectorWord.objects.filter(word_text=daily_word.word_text)[0].word_vec
+  target_words = VectorWord.objects.filter(word_text=word)
+
+  if len(target_words) == 0:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  target_word_vector = target_words[0].word_vec
+
+  #typecast
+  daily_word_vector = np.array(daily_word_vector).astype('float64')
+  target_word_vector = np.array(target_word_vector).astype('float64')
+
+  vscore = score(daily_word_vector, target_word_vector)
+  obj = {
+    "word": word,
+    "score": vscore,
+    "rank": None
+  }
+  return Response(obj)
